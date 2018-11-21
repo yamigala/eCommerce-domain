e4a07c0e97e5e641260e4928502fa232849ad0b5
yamigala <galayamini13@gmail.com>
2018-11-21 19:11:34 +0530

    validations added

--- src/app/order/add/add.component.ts
+++ src/app/order/add/add.component.ts
@@ -23,22 +23,33 @@ export class AddComponent implements OnInit {
   public orders: order[];
   public orderList: order[];
   /**
+   * declare regular expression
+   */
+  public characterRegx : string;
+  public numberRegx : string;
+  /**
    *
    * @param service : injecting order service
    * @param fb :injecting formbuilder
    */
-  constructor(private service: OrderService, private fb: FormBuilder) {}
+  constructor(private service: OrderService, private fb: FormBuilder) {
+    this.characterRegx = '^[a-zA-Z \-\]+';
+  this.numberRegx = '^[0-9]*$';
+  }
   /**
    * using group property of formbuilder
    */
+  // public get itemName(){
+  //   return this.orderDetails.get('itemName');
+  // }
   ngOnInit() {
     this.orderDetails = this.fb.group({
-      itemName: ["", Validators.required],
-      name: ["", [Validators.required]],
+      itemName: ["", [Validators.required]],
+      name: ["",[Validators.required,Validators.minLength(5)]],
       noOfItem: ["", Validators.required],
-      price: ["", Validators.required],
-      address: ["", Validators.required],
-      phoneNo: ["", Validators.required]
+      price: ["", [Validators.required,Validators.pattern(this.numberRegx)]],
+      address: ["", [Validators.required,Validators.pattern(this.characterRegx)]],
+      phoneNo: ["", [Validators.required,Validators.maxLength(10)]],
     });
   }
   /**
